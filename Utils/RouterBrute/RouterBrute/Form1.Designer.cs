ee5f3691252ef79e9d5490b37115bc2553a95b8a
PSmith0000 <40529988+PSmith0000@users.noreply.github.com>
2019-01-19 20:10:24 -0500

    Router Dictionary Attack
    RouterBrute

---
+++ Utils/RouterBrute/RouterBrute/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RouterBrute
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BruteBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BruteBtn
+            // 
+            this.BruteBtn.Location = new System.Drawing.Point(12, 26);
+            this.BruteBtn.Name = "BruteBtn";
+            this.BruteBtn.Size = new System.Drawing.Size(156, 36);
+            this.BruteBtn.TabIndex = 0;
+            this.BruteBtn.Text = "Brute";
+            this.BruteBtn.UseVisualStyleBackColor = true;
+            this.BruteBtn.Click += new System.EventHandler(this.BruteBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Router Login";
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(12, 68);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(362, 40);
+            this.LoadBtn.TabIndex = 4;
+            this.LoadBtn.Text = "Load List";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 121);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BruteBtn);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BruteBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LoadBtn;
+    }
+}
+
